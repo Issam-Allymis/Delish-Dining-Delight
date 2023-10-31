fcf34840ec158bbe12ba2f280d963d6e181d2413
Issam Allymis <issamally0@gmail.com>
2023-10-31 18:51:56 +0000

    Designed and redesigned signup, login, logout, index, about and post_detail(1)html page, wired up the urls for about.html.

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from . import views
 from django.urls import path
+from .views import AnotherPostDetail
 
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
+    path('another_post/<slug>/', AnotherPostDetail.as_view(), name='another_post_detail'),
 ]
