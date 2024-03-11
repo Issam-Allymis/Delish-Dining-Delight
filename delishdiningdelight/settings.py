bcc968112c01b9fa5ea416689840a1ca28b09f1d
Issam Allymis <issamally0@gmail.com>
2024-03-11 15:18:41 +0000

    troubleshooting

--- delishdiningdelight/settings.py
+++ delishdiningdelight/settings.py
@@ -147,11 +147,11 @@ if 'test' in sys.argv:
     }
 
 
-CLOUDINARY_STORAGE = {
+"""CLOUDINARY_STORAGE = {
     'CLOUD_NAME': config('CLOUD_NAME'),
     'API_SECRET': config('API_SECRET'),
     'API_KEY': config('API_KEY'),
-}
+}"""
 
 
 
